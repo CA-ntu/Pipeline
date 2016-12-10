ad929306eee63a8615425cde98a16e79f9e96f8e
Simon <Simon@SimondeMacBook-Pro.local>
2016-12-11 02:04:31 +0800

    CPU.v EX partial

--- CPU.v
+++ CPU.v
@@ -45,9 +45,45 @@ wire			PCWrite;
 wire   			RegWrite;
 wire 			IF_ID_Write;
 wire            Eq;
+wire            RegDst;
+
+wire    [1:0]   ForwardA;
+wire    [1:0]   ForwardB;
+
+
+/* EX */
+
+
+wire    [31:0]  MUX4_out;
+wire    [31:0]  MUX6_out;
+wire    [31:0]  MUX7_out;
+
+wire    [31:0]  RSdata_EX;
+wire    [31:0]  RTdata_EX;
+assign  RSdata_EX = ID_EX_RSdata;
+assign  RTdata_EX = ID_EX_RTdata;
 
 
 
+wire    [4:0]   MUX3_out;
+
+wire    [4:0]   RSaddr_EX;
+wire    [4:0]   RTaddr_EX;
+wire    [4:0]   RDaddr_EX;
+
+assign  RSaddr_EX = ID_EX_RSaddr;
+assign  RTaddr_EX = ID_EX_RTaddr;
+assign  RDaddr_EX = ID_EX_RDaddr;
+
+/* MEM */
+
+wire    [31:0]  ALU_out_MEM;
+
+
+
+/* WB */
+
+wire    [31:0]  MUX5_out;
 
 
 
@@ -55,6 +91,14 @@ wire            Eq;
 /* IF/ID */
 reg 	[31:0]	IF_ID_PC_out;
 reg     [31:0]	IF_ID_inst;
+
+/* ID/EX */
+reg     [31:0]  ID_EX_RSdata;
+reg     [31:0]  ID_EX_RTdata;
+reg     [31:0]  ID_EX_Immediate32;
+reg     [4:0]   ID_EX_RSaddr;
+reg     [4:0]   ID_EX_RTaddr;
+reg     [4:0]   ID_EX_RDaddr;
 
 
 
@@ -113,12 +157,38 @@ Equal Equal(
 );
 
 Adder ADD(
-    .data1_i   (ShiftLeft),
-    .data2_i   (IF_ID_PC_out),
+    .data1_i    (ShiftLeft),
+    .data2_i    (IF_ID_PC_out),
 
     .data_o     (Add_PC_out)
 
 );
+
+
+
+MUX5_2to1 MUX3(
+    .data1_i    (RTdata_EX),
+    .data2_i    (RDaddr_EX),     
+    .select_i   (RegDst),
+    .data_o     (MUX3_out)
+);
+
+MUX32_3to1 MUX6(
+    data1_i     (RSdata_EX),
+    data2_i     (MUX5_out),
+    data3_i     (ALU_out_MEM),
+    select_i    (ForwardA),
+    data_o      (MUX6_out)
+);
+
+MUX32_3to1 MUX7(
+    data1_i     (RTdata_EX),
+    data2_i     (MUX5_out),
+    data3_i     (ALU_out_MEM),
+    select_i    (ForwardB),
+    data_o      (MUX7_out)
+);
+
 
 
 always @(posedge clk_i) begin
@@ -130,6 +200,19 @@ always @(posedge clk_i) begin
 	// temp
 	IF_ID_inst <= inst;
 	IF_ID_PC_out <= PC_out;
+
+    /* ID/EX */
+    /*
+        TODO : Control unit
+    */
+    // temp
+    ID_EX_RSdata <= RSdata;
+    ID_EX_RTdata <= RTdata;
+    ID_EX_Immediate32 <= Immediate32;
+    ID_EX_RSaddr <= RSaddr;
+    ID_EX_RTaddr <= RTaddr;
+    ID_EX_RDaddr <= RDaddr;
+
 	
 	
 
